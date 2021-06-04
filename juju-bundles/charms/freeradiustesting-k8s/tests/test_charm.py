688412763b4af7d511ca88262c7718f0e2407cd1
eminaktas <eminaktas34@gmail.com>
2021-06-04 13:16:34 +0200

    update

---
+++ juju-bundles/charms/freeradiustesting-k8s/tests/test_charm.py
@@ -0,0 +1,23 @@
+# Copyright 2021 root
+# See LICENSE file for licensing details.
+#
+# Learn more about testing at: https://juju.is/docs/sdk/testing
+
+import unittest
+from unittest.mock import Mock
+
+from charm import FreeradiusK8SCharm
+from ops.testing import Harness
+
+
+class TestCharm(unittest.TestCase):
+    def setUp(self):
+        self.harness = Harness(FreeradiusK8SCharm)
+        self.addCleanup(self.harness.cleanup)
+        self.harness.begin()
+
+    def test_action(self):
+        action_event = Mock()
+        print(action_event)
+        return
+        # self.harness.charm._on_auth_test_action(action_event)
